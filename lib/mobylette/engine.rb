a282b60b869b843105308f7ce54c119a954a1a7c
Tiago Scolari <tscolari@gmail.com>
2011-09-02 10:55:36 -0300

    Using ::Rails instead of Rails in the engine.rb

--- lib/mobylette/engine.rb
+++ lib/mobylette/engine.rb
@@ -6,7 +6,7 @@ module Mobylette
 
         # The view helpers have little differences when using sprockets or rails javascript and stylesheet
         # original methods
-        if (Rails.version >= "3.1.0" && Rails.application.config.assets.enabled)
+        if (::Rails.version >= "3.1.0" && ::Rails.application.config.assets.enabled)
           ::ActionController::Base.helper Mobylette::Controllers::Helpers::Sprockets
         else
           ::ActionController::Base.helper Mobylette::Controllers::Helpers::Rails
