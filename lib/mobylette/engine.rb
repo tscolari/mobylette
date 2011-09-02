d07e45d130afdcbc0b8a2fc54c46e9ab200275b3
Tiago Scolari <tscolari@gmail.com>
2011-09-02 10:52:34 -0300

    Different configurations = different includes.

--- lib/mobylette/engine.rb
+++ lib/mobylette/engine.rb
@@ -3,7 +3,14 @@ module Mobylette
     initializer :mobylette do
       ActiveSupport.on_load(:action_controller) do
         ::ActionController::Base.send(:include, Mobylette::Controllers::RespondToMobileRequests)
-        ::ActionController::Base.helper Mobylette::Controllers::Helpers
+
+        # The view helpers have little differences when using sprockets or rails javascript and stylesheet
+        # original methods
+        if (Rails.version >= "3.1.0" && Rails.application.config.assets.enabled)
+          ::ActionController::Base.helper Mobylette::Controllers::Helpers::Sprockets
+        else
+          ::ActionController::Base.helper Mobylette::Controllers::Helpers::Rails
+        end
       end
     end
   end
