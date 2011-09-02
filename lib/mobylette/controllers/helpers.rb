10e55055d1c32cc8d942e13acf55cbc71c1b952b
Tiago Scolari <tscolari@gmail.com>
2011-09-02 20:49:43 -0300

    Documentation added

--- lib/mobylette/controllers/helpers.rb
+++ lib/mobylette/controllers/helpers.rb
@@ -1,8 +1,18 @@
 module Mobylette
   module Controllers
+
+    #
+    # Mobylette::Controllers::Helpers include few methods to
+    # include different css/js files for the mobile and for 
+    # the normal version of your layout
+    #
+    # Personal note: I'm rethinking and I guess that, since you
+    # may have (and probably will) a different layout file for the
+    # "normal" and mobile version, you probably wont use this at all
     module Helpers
       extend ActiveSupport::Concern
 
+      # Adds a "_mobile" sulfix to the files you include (when the request is mobile)
       def mobylette_stylesheet_link_tag(*sources)
         options = sources.extract_options!
         if is_mobile_request?
@@ -12,6 +22,7 @@ module Mobylette
         end
       end
 
+      # Adds a "_mobile" sulfix to the files you include (when the request is mobile)
       def mobylette_javascript_include_tag(*sources)
         options = sources.extract_options!
         if is_mobile_request?
@@ -23,6 +34,9 @@ module Mobylette
 
       private
 
+      #
+      # Anex the "_mobile" sulfix to each string in the array,
+      # before the .#{extension}, if it exists
       def sulfix_mobile_assets(sources, extension)
         sources.each_index do |index|
           source = sources[index]
