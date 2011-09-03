abcea1613c8215b6d2911147b586b5f3db7067c4
Tiago Scolari <tscolari@gmail.com>
2011-09-03 15:34:49 -0300

    Changing the multiple arguments passing for compatibility to ruby 1.8

--- lib/mobylette/controllers/helpers.rb
+++ lib/mobylette/controllers/helpers.rb
@@ -3,7 +3,7 @@ module Mobylette
 
     #
     # Mobylette::Controllers::Helpers include few methods to
-    # include different css/js files for the mobile and for 
+    # include different css/js files for the mobile and for
     # the normal version of your layout
     #
     # Personal note: I'm rethinking and I guess that, since you
@@ -16,9 +16,9 @@ module Mobylette
       def mobylette_stylesheet_link_tag(*sources)
         options = sources.extract_options!
         if is_mobile_request?
-          stylesheet_link_tag(*sulfix_mobile_assets(sources, :css), options)
+          stylesheet_link_tag(*sulfix_mobile_assets(sources, :css).insert(-1, options))
         else
-          stylesheet_link_tag(*sources, options)
+          stylesheet_link_tag(*sources.insert(-1, options))
         end
       end
 
@@ -26,9 +26,9 @@ module Mobylette
       def mobylette_javascript_include_tag(*sources)
         options = sources.extract_options!
         if is_mobile_request?
-          javascript_include_tag(*sulfix_mobile_assets(sources, :js), options)
+          javascript_include_tag(*sulfix_mobile_assets(sources, :js).insert(-1, options))
         else
-          javascript_include_tag(*sources, options)
+          javascript_include_tag(*sources.insert(-1, options))
         end
       end
 
@@ -38,8 +38,8 @@ module Mobylette
       # Anex the "_mobile" sulfix to each string in the array,
       # before the .#{extension}, if it exists
       def sulfix_mobile_assets(sources, extension)
-        sources.map do |source| 
-          if source =~ /.#{extension}/ 
+        sources.map do |source|
+          if source =~ /.#{extension}/
             "#{source.split(/.#{extension}$/)[0]}_mobile.#{extension}"
           else
             "#{source}_mobile"
