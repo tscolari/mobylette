3bcf8699deb688e83d2a6b7ba32731673bc85d51
Tiago Scolari <tscolari@gmail.com>
2011-09-04 01:58:07 -0300

    Adding the documentation history to the project and metric_fu data

---
+++ v1.0.0/metric_fu/output/reek.js
@@ -0,0 +1,14 @@
+              var g = new Bluff.Line('graph', "1000x600");
+      g.theme_37signals();
+      g.tooltips = true;
+      g.title_font_size = "24px"
+      g.legend_font_size = "12px"
+      g.marker_font_size = "10px"
+
+        g.title = 'Reek: code smells';
+        g.data('Duplication', [3])
+g.data('IrresponsibleModule', [1])
+g.data('LowCohesion', [4])
+
+        g.labels = {"0":"9/4"};
+        g.draw();
