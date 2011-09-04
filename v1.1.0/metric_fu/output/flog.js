bdc7f7ff06b13e895ae4ac18c3ce48864255f5f9
Tiago Scolari <tscolari@gmail.com>
2011-09-04 15:32:46 -0300

    Adding v1.1.0 documentation

---
+++ v1.1.0/metric_fu/output/flog.js
@@ -0,0 +1,12 @@
+              var g = new Bluff.Line('graph', "1000x600");
+      g.theme_37signals();
+      g.tooltips = true;
+      g.title_font_size = "24px"
+      g.legend_font_size = "12px"
+      g.marker_font_size = "10px"
+
+        g.title = 'Flog: code complexity';
+        g.data('average', [6.170522209788225,6.280482409477331]);
+        g.data('top 5% average', [22.24909577429176,26.31890267321719])
+        g.labels = {"0":"9/3","1":"9/4"};
+        g.draw();
